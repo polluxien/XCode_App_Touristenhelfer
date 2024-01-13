c31f042653f3d4da651ec7ce24c22ecd1d8f5d1a
Bennet <s86545@bht-berlin.de>
2024-01-13 13:27:10 +0100

    Initial Commit

---
+++ Touristenhelfer/TouristenhelferApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouristenhelferApp.swift
+//  Touristenhelfer
+//
+//  Created by Bennet Panzek on 13.01.24.
+//
+
+import SwiftUI
+
+@main
+struct TouristenhelferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
